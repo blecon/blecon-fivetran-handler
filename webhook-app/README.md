270695ac4c5fa50579157af623ed59688f632f00
danros <dan.ros@blecon.net>
2022-12-06 15:53:16 +0000

    Create fivetran adapter

--- webhook-app/README.md
+++ webhook-app/README.md
@@ -1,2 +1,11 @@
-# webhook-app
-This is the base for AWS Lambda Python based examples for Blecon.
+# Blecon Lambda Examples
+AWS Lambda Python based examples for Blecon.
+
+To deploy this to your AWS account, run 
+
+    sam build
+    sam deploy --guided 
+
+in the webhook-app directory.
+
+To set up SAM, follow [this guide](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-sam-cli.html) including the prerequisites.
--- webhook-app/process_request/app.py
+++ webhook-app/process_request/app.py
@@ -1,59 +1,23 @@
-import boto3
-import json
 import os
-import urllib.parse
-from datetime import datetime
-
-from boto3.dynamodb.conditions import Key
-
-
-def get_requests(event):
-    identifier = event['path']
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-    response = table.query(
-        KeyConditionExpression=Key('InstanceID').eq(identifier)
-    )
-
-    if not response.get('Items'):
-        return {"statusCode": 404}
-    requests = [i for i in response.get('Items')]
-
-    return dict(statusCode=200, body=json.dumps(requests))
-
-
-def post_request(event):
-    print(event)
-    response = None
-    identifier = event['path']
-    if event.get('queryStringParameters'):
-        response = event['queryStringParameters'].get('response_json')
-        if response:
-            response = urllib.parse.unquote(response)
-
-    body = json.loads(event['body'])
-
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-    table.put_item(
-        Item={
-            'InstanceID': identifier,
-            'RequestTimestamp': str(datetime.now().isoformat()),
-            'request': json.dumps(body),
-        }
-    )
-    json_resp = json.dumps(body)
-    if response:
-        json_resp = response
-
-    return dict(statusCode=200, body=json_resp)
+import json
+import requests
 
 
 def lambda_handler(event, context):
-    print(event['httpMethod'])
-    if event['httpMethod'] == "GET":
-        return get_requests(event)
-    else:
-        return post_request(event)
+    # forward request
+    body = json.loads(event['body'])
+    events = []
+    for ev in body['request_data']['records']:
+        event = {
+            'device_id': body['network_headers']['device_id'],
+            'network_id': body['network_headers']['network_id'],
+            'account_id': body['network_headers']['account_id'],
+            'timestamp': ev['timestamp'],
+            'event_type': ev['event_type'],
+            'event_value': ev['event_value'],
+        }
+        events.append(event)
+
+    requests.post(os.environ.get('HANDLER'), json=events)
+    print("Posted {} to {}".format(events, os.environ.get('HANDLER')))
+    return dict(statusCode=200)
